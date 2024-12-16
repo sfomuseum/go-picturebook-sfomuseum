c5124fe7d2d366137e5c403fadc573b1f3a432fe
sfomuseumbot <sfomuseumbot@localhost>
2024-12-16 13:04:10 -0800

    add cmd/ tools

---
+++ cmd/picturebook/main.go
@@ -0,0 +1,23 @@
+// picturebook is a command-line application for creating a PDF file from a folder containing images.
+package main
+
+import (
+	"context"
+	"log"
+
+	_ "github.com/sfomuseum/go-picturebook-shoebox"
+	_ "gocloud.dev/blob/fileblob"
+
+	"github.com/aaronland/go-picturebook/app/commandline"
+)
+
+func main() {
+
+	ctx := context.Background()
+	err := commandline.Run(ctx)
+
+	if err != nil {
+		log.Fatalf("Failed to run picturebook application, %w", err)
+	}
+
+}
